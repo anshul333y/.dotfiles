f1d15e3d90e35a1e147d2b40597c2b337713ef82
Anshul Yadav <anshul333y@gmail.com>
2025-10-02 23:54:17 +0530

    added wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+local home = os.getenv("HOME")
+wezterm.add_to_config_reload_watch_list(home .. "/.cache/wal/wezterm-wal.toml")
+
+return {
+	default_prog = { "/bin/zsh", "-i" },
+
+	color_scheme_dirs = { home .. "/.cache/wal" },
+	color_scheme = "wezterm-wal",
+
+	font = wezterm.font("Maple Mono NF CN"),
+	font_size = 14,
+
+	window_background_opacity = 0.8,
+	initial_cols = 145,
+	initial_rows = 36,
+
+	adjust_window_size_when_changing_font_size = false,
+}
