83935b1994a0ad4cc3c88e9520f68ead8d95538a
Anshul Yadav <anshul333y@gmail.com>
2025-12-28 22:09:51 +0530

    changed some options

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -18,15 +18,15 @@ return {
     },
   },
 
-  {
-    "RedsXDD/neopywal.nvim",
-    name = "neopywal",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      local neopywal = require("neopywal")
-      neopywal.setup()
-      vim.cmd.colorscheme("neopywal")
-    end,
-  },
+  -- {
+  --   "RedsXDD/neopywal.nvim",
+  --   name = "neopywal",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     local neopywal = require("neopywal")
+  --     neopywal.setup()
+  --     vim.cmd.colorscheme("neopywal")
+  --   end,
+  -- },
 }
